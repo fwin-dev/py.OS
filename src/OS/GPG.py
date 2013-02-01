f9c26be3d5fb06ea52831ae68ae960d0e1887b65
Jesse Cowles <jcowles@indigital.net>
2013-02-01 16:26:03 -0500

    changed GPG module to not use py.Disk since it was barely using it

--- src/OS/GPG.py
+++ src/OS/GPG.py
@@ -1,13 +1,14 @@
 import OS
-from LinuxCommon.Common import Disk
+
+import os, os.path
 
 class GPG(object):
 	def import_(self, keyFile):
 		keyFile = str(keyFile)
 		OS.runCMD("gpg --batch --import %s", keyFile)
-		id = self.getKeyID(keyFile)
+		id_ = self.getKeyID(keyFile)
 		# LIMITATION: if key is already signed, gnupg prints out key information to terminal even if stdout is connected to pipe
-		# OS.runCMD("gpg --lsign-key " + id)
+		# OS.runCMD("gpg --lsign-key " + id_)
 		# LIMITATION: owner trust of key can't be set non-interactively for it due to limitations in the gnupg interface
 	
 	def isKeyImported(self, id):
@@ -15,35 +16,31 @@ class GPG(object):
 		return id + ":" in OS.runCMD("gpg --list-secret-keys --with-colons").stdout
 	
 	def getKeyID(self, filename):
-		if isinstance(filename, Disk.base.PathAbstract):
-			assert isinstance(filename, Disk.Local.FilePath)
-			filename = str(filename)
+		filename = str(filename)
 		
-		id = None
+		id_ = None
 		for line in OS.runCMD("gpg --with-colons %s", filename).stdout.split("\n"):
 			line = line.split(":")
 			if line[0] == "pub":
-				if id != None:
+				if id_ != None:
 					raise Exception("More than 1 key found in " + filename)
-				id = line[4][8:]
-		return id
+				id_ = line[4][8:]
+		return id_
 	
 	def encrypt(self, inputFilePath, keyID):
-		if isinstance(inputFilePath, str) or isinstance(inputFilePath, unicode):
-			inputFilePath = Disk.Local.FilePath(inputFilePath)
+		inputFilePath = str(inputFilePath)
 		outputFilePath = inputFilePath + ".pgp"
-		if outputFilePath.exists():
-			outputFilePath.remove()
+		if os.path.exists(outputFilePath):
+			os.remove(outputFilePath)
 		OS.runCMD("gpg --batch -r %s --output %s --encrypt %s", (keyID, outputFilePath, inputFilePath))
 		return outputFilePath
 	
 	def decrypt(self, inputFilePath, password):
-		if isinstance(inputFilePath, str) or isinstance(inputFilePath, unicode):
-			inputFilePath = Disk.Local.FilePath(inputFilePath)
-		assert str(inputFilePath).endswith(".pgp")
-		outputFilePath = inputFilePath.splitext()[0]
-		if outputFilePath.exists():
-			outputFilePath.remove()
+		inputFilePath = str(inputFilePath)
+		assert inputFilePath.endswith(".pgp")
+		outputFilePath = os.path.splitext(inputFilePath)[0]
+		if os.path.exists(outputFilePath):
+			os.remove(outputFilePath)
 		OS.runCMD("gpg --batch --passphrase %s --output %s --decrypt %s", (password, outputFilePath, inputFilePath))
 		return outputFilePath
 		
