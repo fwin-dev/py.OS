c0a0ca8c49e490da1f3e94005cf3310e507c5a60
Jesse Cowles <jcowles@indigital.net>
2013-02-01 16:31:21 -0500

    checkSuitablePyExe now can return the full system path to a suitable python version instead of just the executable name, in case the executable name does not lie in the system path

--- src/OS/PyVersion.py
+++ src/OS/PyVersion.py
@@ -1,23 +1,38 @@
 import _OS
 
-import sys, os
+import sys, os, platform
 
-def checkSuitablePyExe(ver=(2,6), assertExists=True):
-	currentVer = [int(i) for i in _OS.runCMD("python -V").stderr.split("\n")[0].replace("Python ", "").split(".")]
-	if currentVer < ver:
+"""
+This module can also be invoked as a script from the terminal, with 2 purposes.
+
+If no arguments are passed, it will print the name or path of the current python executable being invoked.
+If a major.minor version is passed, it will try to locate the name/path of that installed python version.
+	If no such version exists, 'None' will be printed out.
+"""
+
+def checkSuitablePyExe(ver=platform.python_version_tuple[:2], assertExists=True):
+	"""
+	Returns the executable name or path of the wanted python version such that it can be executed from the terminal.
+	
+	This function can be used to ensure compatibility between different versions.
+	"""
+	currentVer = platform.python_version_tuple[:2]
+	if currentVer < ver or currentVer[0] > ver[0]:
+		# incompatible, so check for a differently installed python version
 		exe = "python" + str(ver[0]) + "." + str(ver[1])
 		if _OS.runCMD(exe + " -V", assertSuccess=False).returnCode != 0:
 			if assertExists:
-				raise Exception("Your python version is too old. Please install python " + ".".join(ver) + " and run this application using that version.")
+				raise Exception("Your python version is incompatible. Please install python " + ".".join(ver) + " and run this application using that version.")
+			return None
 		else:
 			return exe
 	else:
-		return "python"
+		return sys.executable
 
 PY_EXE = checkSuitablePyExe()
 
 if __name__ == "__main__":
 	if len(sys.argv) == 1:
-		print("\n=== Found py exe '" + PY_EXE + "'")
+		print(PY_EXE)
 	else:
 		os.system(PY_EXE + " " + " ".join(sys.argv[1:]))
